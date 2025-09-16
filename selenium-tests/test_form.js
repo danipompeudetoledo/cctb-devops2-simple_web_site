9f24a406575d947d59ca8e4308bfa1e19aca899a
Danilo Santos <92264313+danipompeudetoledo@users.noreply.github.com>
2025-09-15 17:27:35 -0700

    Create test_form.js

---
+++ selenium-tests/test_form.js
@@ -0,0 +1,29 @@
+
+const { Builder, By, until } = require('selenium-webdriver');
+const chrome = require('selenium-webdriver/chrome');
+
+(async function testForm() {
+  const TESTING_URL = 'http://54.87.136.54'; 
+  let options = new chrome.Options()
+    .addArguments('--headless=new', '--no-sandbox', '--disable-dev-shm-usage');
+
+  let driver = await new Builder().forBrowser('chrome').setChromeOptions(options).build();
+  try {
+    await driver.get(http://54.87.136.54);
+
+    await driver.findElement(By.id('name')).sendKeys('Danilo');
+    await driver.findElement(By.id('email')).sendKeys('danilodias@gmail.com');
+    await driver.findElement(By.id('role')).sendKeys('Developer');
+
+   
+    await driver.findElement(By.css('button[type="submit"]')).click();
+
+    
+    const ok = await driver.wait(until.elementLocated(By.css('.msg.success')), 5000);
+    await driver.wait(until.elementTextContains(ok, 'Registration submitted successfully!'), 3000);
+
+    console.log('Test Success');
+  } finally {
+    await driver.quit();
+  }
+})();
